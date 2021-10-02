81083233ba8a4afe30323303be97194e4b35d4da
Sarah Fuoss <sfuoss@gmail.com>
2021-10-02 11:35:23 -0500

    Uninstalled Bootstrap in favor of material-ui.

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,4 +1,4 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+
 
 function AddMovie() {
     return (
@@ -8,17 +8,6 @@ function AddMovie() {
                 <input type="text" placeholder="Title" />
                 <input type="text" placeholder="Poster URL" />
                 <input type="text" placeholder="movie Description" />
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Dropdown Button
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
             </form>
         </>
     );
