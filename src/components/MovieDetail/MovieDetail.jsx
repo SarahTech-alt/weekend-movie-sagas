308139d51b88db0e7fe7f32f7c1d244db7c72bf9
Sarah Fuoss <sfuoss@gmail.com>
2021-10-01 20:57:04 -0500

    On MovieDetail accessed reducers from the store and mapped them to display on the DOM

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,35 @@
+import { useSelector } from 'react-redux';
+
 function MovieDetail() {
+    // access movie details from the store
+    const movieDetailsToDisplay = useSelector(store => store.selectedMovieDetails);
+    // access movie genres from the store
+    const movieGenresToDisplay = useSelector(store => store.selectedMovieGenres);
+
     return (
         <>
-
+            <main>
+                {/* <p>{JSON.stringify(movieDetailsToDisplay)}</p> */}
+                <section className="detail">
+                    {movieDetailsToDisplay.map(movie => {
+                        return (
+                            <div key={movie.id} >
+                                <h3>{movie.title}</h3>
+                                <img src={movie.poster} alt={movie.title} />
+                                <p>{movie.description}</p>
+                            </div>
+                        );
+                    })}
+                    <p>Genre(s):</p>
+                    {movieGenresToDisplay.map(genre => {
+                        return (
+                            <div key={genre.id} >
+                                <p>{genre.name}</p>
+                                </div>
+                        );
+                    })}
+                </section>
+            </main>
         </>
     );
 }
